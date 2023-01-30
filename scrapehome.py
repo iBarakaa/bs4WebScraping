8399a28207c8ea3089a19ae5691d1bb78301abb9
iBarakaa <isaacbaraka1@gmail.com>
2023-01-30 15:46:07 +0300

    Introduction to bs4 scraping by grabbing courses and pricing

---
+++ scrapehome.py
@@ -0,0 +1,37 @@
+# beautifulsoup4 library installation
+# method specifier to parse html code 
+# hence we install lxml parser library
+
+# importing beautiful soup
+from bs4 import BeautifulSoup
+
+#accessing content inside home.html file
+#second entry either decides whether we read, write or both
+with open('home.html', 'r') as html_file: 
+    content = html_file.read()
+
+    #using beautiful soup to prettify html and work with its tags like python objects
+    soup = BeautifulSoup(content, 'lxml')
+    #print(soup.prettify()) #outputs read file
+    
+    #displaying all the courses?
+    # courses_html_tags = soup.find_all('h5')
+    # for course in courses_html_tags:
+    #     print(course.text)
+
+    #grabbing specific information
+    #.find searches for specified tags
+    #tags = soup.find('h5') #searches for the first element and stops execution
+    #tags = soup.find_all('h5')
+    #print(tags)
+
+    #with real websites, we have to use inspect of browsers
+
+    # grabbing courses and prices
+    course_cards = soup.find_all('div', class_='card') #Storage of all course cards
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1] #split helps us procure a specific section of string, ini this case we took the last section
+
+        #printing output with f-string
+        print(f'{course_name} costs {course_price}')
