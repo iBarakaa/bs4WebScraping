5920975944a788a4750c9da5ed96a0e5e38df946
iBarakaa <isaacbaraka1@gmail.com>
2023-02-01 15:15:33 +0300

    Refining shares to scrape ordered information from Stocks.com

--- NSEscrapper/main.py
+++ NSEscrapper/main.py
@@ -1,6 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-from_nse = requests.get('https://www.nse.co.ke/', verify = False).text
+from_marketlist = requests.get('https://live.mystocks.co.ke/m/pricelist')
+soup = BeautifulSoup(from_marketlist.content,'lxml')
 
-soup = BeautifulSoup(from_nse,'lxml')
+#   table of shares
+shares = soup.find_all('tr', class_ = 'row')
+print(shares)
+
+
+for share in shares:
+    inactive = share.find('tr', class_ = 'row r1 inact')
+
+    if not inactive:
+        category = share.find('th', class_ = 'head').text
+        company_name = share.find('td', class_ = 'nm').text
+        share_price = share.find('td', class_ = 'n').text
+
+        
+
+
+         
+
