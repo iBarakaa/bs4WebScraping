eeb617855f60f838bbdeec2a67fc8367ee6a07ad
iBarakaa <isaacbaraka1@gmail.com>
2023-02-01 10:57:41 +0300

    Solved SSL verify failed issue

--- NSEscrapper/main.py
+++ NSEscrapper/main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-from_nse = requests.get('https://www.nse.co.ke/').text
+from_nse = requests.get('https://www.nse.co.ke/', verify = False).text
 
 soup = BeautifulSoup(from_nse,'lxml')
